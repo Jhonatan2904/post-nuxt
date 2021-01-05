219c3d0493449ad4dd8706d425a875886bbd40b0
Jhonatan2904 <jhcastillo@unitru.edu.pe>
2021-01-05 11:54:20 -0500

    onesignal conf 14 new onesignalworker.js

--- plugins/sw-onesignal.js
+++ plugins/sw-onesignal.js
@@ -1,1 +1,9 @@
 console.log("Listo para registrar un sw.")
+
+if (process.env) {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/OneSignalWorker.js', {
+            scope: '/'
+        })
+    }
+}
