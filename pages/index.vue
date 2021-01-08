81e3bd7c9bf0ba4c8dd7b9df0224c3940726bc15
Jhonatan2904 <jhcastillo@unitru.edu.pe>
2021-01-08 14:39:18 -0500

    probando sw

---
+++ pages/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <Header />
+    <PostList :posts="objectPost"/>
+  </div>
+</template>
+
+<script>
+import PostList from '~/components/post/PostList'
+export default {
+  // middleware: 'log',
+  // asyncData(context) {
+    // console.log(context)
+    //     setTimeout(() => {
+    //       callback(null, {
+    //         objectPost: [
+    //         {id: 1, title: 'First Post', text: 'Descripción del primer post', img:'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg'},
+    //         {id: 2, title: 'Second Post', text: 'Descripción del segundo post', img:'https://miro.medium.com/max/1000/0*kBHpKva09AsGj7RQ'}
+    //     ]
+    //       })
+    //     }, 2000);
+  //   return new Promise((resolve, reject)=> {
+  //     setTimeout(() => {
+  //       resolve({
+  //          objectPost: [
+  //           {id: 1, title: 'First Post', text: 'Descripción del primer post', img:'https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg'},
+  //           {id: 2, title: 'Second Post', text: 'Descripción del segundo post', img:'https://miro.medium.com/max/1000/0*kBHpKva09AsGj7RQ'}
+  //       ]
+  //       })
+  //     }, 3000);
+  //   })
+  //   .then(data => {
+  //     return data
+  //   })
+  //   .catch(e => {
+  //     return context.error(new Error())
+  //   })
+  // },
+
+  mounted(){
+    // console.log("Probando OneSignal...");
+    // window.$OneSignal = window.$OneSignal || []
+    // console.log(this.$OneSignal)
+    // this.$OneSignal.push(() => {
+    //   this.$OneSignal.isPushNotificationsEnabled((isEnabled) => {
+    //     if (isEnabled) {
+    //       console.log("Push Notifications is actived.");
+
+    //     } else {
+    //       console.log("Push Notifications is not actived.");
+    //     }
+    //   })
+
+    //   this.$OneSignal.showHttpPermissionRequest(response => {
+    //     console.log("Response: " , response)
+    //   })
+    // })
+  },
+
+  methods: {
+    suscripcion(){
+      // const permiso = Notification.requestPermission(result => {
+      //   console.log(result);
+      // })
+      
+    }
+  },
+
+  computed: {
+    objectPost(){
+      return this.$store.getters.getPosts
+    }
+  },
+  // data(){
+  //   return {
+  //     objectPost: []
+  //   }
+  // },
+  components: {
+    PostList,
+  },
+}
+</script>
+<style>
+
+</style>
