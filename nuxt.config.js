a01e0fd557c00e566b14ddb31efb7bdf24fd6481
Jhonatan2904 <jhcastillo@unitru.edu.pe>
2021-01-11 12:22:47 -0500

    pwa config bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,16 @@ export default {
     ],
     script: [
       { src: 'https://www.google.com/recaptcha/api.js?onload=vueRecaptchaApiLoaded&render=explicit' },
-      {src: 'https://cdn.onesignal.com/sdks/OneSignalSDK.js'}
+      { src: 'https://cdn.onesignal.com/sdks/OneSignalSDK.js' },
+      {
+        src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js'
+      },
+      {
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js'
+      },
+      {
+        src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js'
+      }
     ]
   },
 
